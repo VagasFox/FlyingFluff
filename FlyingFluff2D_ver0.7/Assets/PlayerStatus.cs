90a4e8f97e7c5f68b6f71b18bc4d580e02b124a4
VagasFox <gargoyle710@gmail.com>
2016-05-06 22:21:37 +0900

    Added files via upload
    初アップロード

---
+++ FlyingFluff2D_ver0.7/Assets/PlayerStatus.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerStatus : MonoBehaviour {
+	public bool bubble_Active = false;	//シャボン玉がAciveかどうか
+	private GameObject bubbleObj;		//シャボン玉Object
+
+	// Use this for initialization
+	void Awake () {
+		bubbleObj = transform.Find ("PlayerBubble").gameObject;
+		if (bubbleObj.activeSelf == true) bubbleObj.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collider2D col){
+		//もしシャボン玉が当たったら
+		if(col.name == ""){
+			
+		}
+	}
+}
---
+++ FlyingFluff2D_ver0.7/Assets/Script/VerticalWind.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VerticalWind : MonoBehaviour {
+	public bool DownFlag;
+	public float g_Scale = 0.5f;
+	public float g_Default;
+	bool updateFlag;
+
+	// Use this for initialization
+	void Start () {
+		updateFlag = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+		
+	void  OnTriggerStay2D(Collider2D col){
+		if (col.CompareTag("Player")) {
+			if (updateFlag != true) {
+				g_Default = col.GetComponent<Rigidbody2D> ().gravityScale;
+				updateFlag = true;
+			}
+
+			if (DownFlag != true) {
+				Debug.Log ("hit up");
+				g_Scale = 0.6f;
+				col.GetComponent<Rigidbody2D> ().gravityScale -= g_Scale *Time.deltaTime;
+			} else {
+				Debug.Log ("hit down");
+				g_Scale = 10f;
+				col.GetComponent<Rigidbody2D> ().gravityScale += g_Scale *Time.deltaTime;
+			}
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		if (col.CompareTag("Player")) {
+			col.GetComponent<Rigidbody2D> ().gravityScale = g_Default;
+			updateFlag = false;
+		}
+	}
+}
